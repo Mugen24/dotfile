54caa49f763eb2335d322460d0510431743fc9c6
John Nguyen <John.nguyen1022@gmail.com>
2022-05-08 08:17:14 +1000

    Service working

--- notification/battery.c
+++ notification/battery.c
@@ -1,6 +1,9 @@
+#define _XOPEN_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #define BATTERY_PATH "/sys/class/power_supply/BAT0"
 
@@ -40,10 +43,16 @@ char *get_content(char *file_path){
     FILE *fp;
     fp = fopen(file_path, "rb");
     if ( fp == NULL ){
+        printf("%s\n", file_path);
         perror("Error: ");
+        exit(0);
     }
     unsigned int size = (unsigned int)getFilesize(file_path);
-    char *my_string = malloc(sizeof(char) * size + 1);
+    char *my_string = calloc(sizeof(char) * size + 1, 1);
+    if (my_string == NULL){
+        puts("Not enough space");
+        exit(-1);
+    }
     while (fread(my_string, sizeof(char), size/2, fp) == size/2){
 
     }
@@ -53,19 +62,71 @@ char *get_content(char *file_path){
 
 
 Battery_stat init_battery(){
-    Battery_stat temp;   
-    temp = malloc(sizeof(struct _battery_stat));
-    return temp;
+    Battery_stat my_battery;
+    my_battery = malloc(sizeof(struct _battery_stat));
+
+    return my_battery;
+}
+
+void reload_battery(Battery_stat my_battery){
+    char prefix[][20] = {"/capacity", "/status"};
+    const unsigned int len = sizeof(prefix) / sizeof(prefix[0]);
+    for (int stat = 0; stat < len; stat++){
+        char capacity[strlen(BATTERY_PATH) + strlen(prefix[stat]) + 1];
+        capacity[0] = 0;
+        strcat(capacity, BATTERY_PATH);
+        strcat(capacity, prefix[stat]);
+        char *cap = get_content(capacity);
+        rstrip(cap);
+        if (strcmp(prefix[stat], "/status") == 0){
+            strcpy(my_battery->status, cap);
+        }
+        else if (strcmp(prefix[stat], "/capacity") == 0){
+            my_battery->battery_level = atoi(cap);
+        }
+        free(cap);
+    }
+}
+
+void free_bat(Battery_stat my_battery){
+    free(my_battery);
+}
+
+    //os.environ['DBUS_SESSION_BUS_ADDRESS'] = "unix:path=/run/dbus/system_bus_socket"
+    //os.environ["DISPLAY"] = ":0"
+    
+void notify(char *text){
+    system("systemctl --user import-environment DISPLAY WAYLAND_DISPLAY SWAYSOCK");
+    //char notify[] = "notify-send";
+    //putenv("DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket");
+    char notify[] = "notify-send";
+    char *notify_string = malloc(sizeof(char) * (strlen(notify) + strlen(text) + 1 + 1));
+    notify_string[0] = 0;
+    strcat(notify_string, notify);
+    strcat(notify_string, " ");
+    strcat(notify_string, text);
+    puts(notify_string);
+    system(notify_string);
+    free(notify_string);
+}
+
+void handler(){
+    write(1, "Shutting down battery notification", 45);
 }
 
 int main(){
-    char prefix[] = "/capacity";
-    char capacity[strlen(BATTERY_PATH) + strlen(prefix) + 1];
-    strcat(capacity, BATTERY_PATH);
-    strcat(capacity, prefix);
-    char *cap = get_content(capacity);
-    rstrip(cap);
-    printf("%s\n", cap);
-    free(cap);
+    signal(SIGINT, handler);
+    Battery_stat my_battery = init_battery();
+    char str[5];
+    while(1){
+        reload_battery(my_battery);
+        int length = snprintf(NULL, 0, "%d", my_battery->battery_level);
+        snprintf( str, length + 1 , "%d", my_battery->battery_level);
+        notify(my_battery->status);
+        notify(str);
+        sleep(10);
+    }
+    free_bat(my_battery);
+    
     return 0;
 }
--- notification/test.c
+++ notification/test.c
@@ -1,38 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-void rstrip(char *string){
-    unsigned long len = strlen(string);
-    for (int i = len - 1; i >= 0; i--){
-        if (string[i] != '\n'){
-            printf("what?: ");
-            printf("%d\n", string[i]);
-            break;
-        }
-        else {
-            string[i] = 0;
-        }
-    }
-}
-
-int main(){
-    FILE *ptr;
-    char buffer[100];
-
-    ptr = fopen("test.txt", "r");
-
-    if (NULL == ptr){
-        printf("smth wrong");
-    }
-
-    while (!feof(ptr)){
-        fread(buffer, sizeof(buffer), 1, ptr);
-    }
-    printf("%s", buffer);
-    for (int i = 0; i < strlen(buffer); i++){
-        printf("%c ", buffer[i]);
-    }
-    rstrip(buffer);
-    printf("%s", buffer);
-    fclose(ptr);
-    return 1;
+void main(){
+    eval("echo hello");
 }
